bf794742c6ce34e17bebc1fa2179be76e65ec273
saeideh eslamian <115387823+saeideh-eslamian@users.noreply.github.com>
2023-02-16 13:00:31 +0100

    add score for players in game

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ import random
 
 #  ---------------functions--------------------------
 
-
 def next_move(row, column):
     global player
 
 
-
-    if (buttons[row][column])['text'] == "" and fill() == False:
+    if (buttons[row][column])['text'] == "" and check_winner()==False :
         (buttons[row][column])['text'] = player
 
-        if check_winner() == False and fill() == False:
+        if check_winner() :
+            label_player.config(text=player + ' win')
+            counter_win()
+
+        elif fill() == False:
 
             if player == players[0]:
                 player = players[1]
@@ -23,16 +25,12 @@ def next_move(row, column):
                 player = players[0]
             label_player['text'] = 'Player:' + player + " "
 
-        elif check_winner() == False and fill() == True:
+        elif fill():
             label_player['text'] = "Tie"
             count_tie = counter_tie.get()
             count_tie += 1
             counter_tie.set(count_tie)
             label_tie['text'] = 'tie:' + str(count_tie) + " "
-
-    elif check_winner():
-
-        label_player.config(text=player + ' win')
 
 
 def fill():
@@ -46,20 +44,15 @@ def fill():
         return True
 
 def counter_win():
-    print('input')
     if player == players[0]:
-        print('player o')
         counter_o = counter_player_o.get()
         counter_o += 1
-        print(counter_o)
         counter_player_o.set(counter_o)
         label_player_o['text'] = 'o:' + str(counter_o) + " "
 
     else:
-        print('player x')
         counter_x = counter_player_x.get()
         counter_x += 1
-        print(counter_x)
         counter_player_x.set(counter_x)
         label_player_x['text'] = 'X:' + str(counter_x) + " "
 
@@ -68,8 +61,6 @@ def check_winner():
     for column in range(3):
         if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
             label_player.config(text=player + ' win')
-            print('case1')
-            counter_win()
             for row in range(3):
                 buttons[row][column].configure(bg = "red")
             return True
@@ -77,29 +68,22 @@ def check_winner():
     for row in range(3):
         if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
             label_player.config(text=player + ' win')
-            print('case2')
-            counter_win()
             for column in range(3):
                 buttons[row][column].configure(bg = "red")
             return True
 
     if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
         label_player.config(text=player + ' win')
-        print('case3')
-        counter_win()
         for item in range(3):
             buttons[item][item].configure(bg="red")
         return True
 
     if buttons[2][0]['text'] == buttons[1][1]['text'] == buttons[0][2]['text'] != "":
         label_player.config(text=player + ' win')
-        print('case4')
-        counter_win()
         buttons[2][0].configure(bg="red")
         buttons[1][1].configure(bg="red")
         buttons[0][2].configure(bg="red")
         return True
-
     return False
 
 
@@ -130,6 +114,7 @@ buttons = [[0, 0, 0],
 counter_player_o = IntVar(value=0)
 counter_player_x = IntVar(value=0)
 counter_tie = IntVar(value=0)
+
 
 # ------ Frame Turn ---------------------
 
